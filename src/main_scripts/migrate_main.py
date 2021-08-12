10f24ad67501319fb2e57c2d201afb98158b3010
Adam Hamilton-Sutherland <ahamiltonsut@paloaltonetworks.com>
2021-08-12 15:33:16 -0700

    Added main scripts and main menu

---
+++ src/main_scripts/migrate_main.py
@@ -0,0 +1,69 @@
+from sdk.load_config import load_config_create_sessions
+from sdk.color_print import c_print
+
+from cloud_accounts import cld_migrate
+from account_groups import ag_main
+from resource_lists import rl_main
+from user_roles import role_sync
+from user_profiles import users_main
+from ip_allow_lists import ip_migrate
+from compliance_standards import cmp_migrate
+from policies import plc_migrate_custom
+from policies import plc_migrate_default
+from alert_rules import ar_main
+from enterprise_settings import settings_migrate
+from anomaly_settings import ano_sync
+
+#PROPER ORDER
+#Cloud accounts
+#Account Groups
+#Resource Lists
+#User Roles
+#Users
+#Trusted IPs
+#Compliance Standards
+#Saved Searches - done by policy
+#Policy
+#Alert Rules
+
+def migrate(modes: dict):
+    tenant_sessions = load_config_create_sessions()
+
+    #CLOUD ACCOUNT MIGRATE
+    if 'cloud' in modes:
+        cld_migrate.migrate_cloud_accounts(tenant_sessions)
+    #ACCOUNT GROUPS MIGRATE
+    if 'account' in modes:
+        ag_main.acc_groups(tenant_sessions)
+    #RESOURCE LIST MIGRATE
+    if 'resource' in modes:
+        rl_main.rl_main(tenant_sessions)
+    #USER ROLES MIGRATE
+    if 'role' in modes:
+        role_sync.sync_roles(tenant_sessions, True, False, False)
+    #USERS MIGRATE
+    if 'user' in modes:
+        users_main.users_main(tenant_sessions)
+    #TRUSTED IP MIGRATE
+    if 'ip' in modes:
+        ip_migrate.migrate_trusted_ips(tenant_sessions)
+    #COMPLIANCE MIGRATE
+    if 'compliance' in modes:
+        cmp_migrate.migrate_compliance_standards(tenant_sessions)
+    #POLICY MIGRATE
+    if 'policy' in modes:
+        plc_migrate_custom.migrate_custom_policies(tenant_sessions)
+        plc_migrate_default.migrate_builtin_policies(tenant_sessions)
+    #ALERT RULES MIGRATE
+    if 'alert' in modes:
+        ar_main.alert_rules(tenant_sessions)
+    if 'anomaly' in modes:
+        ano_sync.sync_anomaly_settings(tenant_sessions, True, False, False)
+    if 'settings' in modes:
+        #Enterprise settings
+        settings_migrate.migrate_settings(tenant_sessions)
+
+
+if __name__ == '__main__':
+    migrate()
+
---
+++ src/main_scripts/sync_main.py
@@ -0,0 +1,50 @@
+from sdk.load_config import load_config_create_sessions
+from sdk.color_print import c_print
+
+from cloud_accounts import cld_sync
+from account_groups import ag_sync_main
+from resource_lists import rl_sync_main
+from user_roles import role_sync
+from user_profiles import users_sync_main
+from ip_allow_lists import ip_sync
+from compliance_standards import cmp_sync
+from saved_search import search_sync
+from policies import plc_sync
+from alert_rules import ar_main
+from anomaly_settings import ano_sync
+from enterprise_settings import settings_migrate
+
+def sync(modes: dict):
+    tenant_sessions = load_config_create_sessions()
+
+    if 'cloud' in modes:
+        cld_sync.sync_cloud_accounts(tenant_sessions, modes['cloud'].get('add', True), modes['cloud'].get('up', True), modes['cloud'].get('del', True))
+    if 'account' in modes:
+        ag_sync_main.ag_sync_main(tenant_sessions, modes['account'].get('add', True), modes['account'].get('up', True), modes['account'].get('del', True))
+    if 'resource' in modes:
+        rl_sync_main.resource_list_sync(tenant_sessions, modes['resource'].get('add', True), modes['resource'].get('up', True), modes['resource'].get('del', True))
+    if 'role' in modes:
+        role_sync.sync_roles(tenant_sessions, modes['role'].get('add', True), modes['role'].get('up', True), modes['role'].get('del', True))
+    if 'user' in modes:
+        users_sync_main.users_main(tenant_sessions, modes['user'].get('add', True), modes['user'].get('up', True), modes['user'].get('del', True))
+    if 'ip' in modes:
+        ip_sync.sync_trusted_ips(tenant_sessions, modes['ip'].get('add', True), modes['ip'].get('up', True), modes['ip'].get('del', True))
+    if 'compliance' in modes:
+        cmp_sync.sync_compliance(tenant_sessions, modes['compliance'].get('add', True), modes['compliance'].get('up', True), modes['compliance'].get('del', True))
+    if 'search' in modes:
+        search_sync.sync_search(tenant_sessions, modes['search'].get('add', True), modes['search'].get('del', True))
+    if 'policy' in modes:
+        plc_sync.sync_policies(tenant_sessions, modes['policy'].get('add', True), modes['policy'].get('up', True), modes['policy'].get('del', True))
+    if 'alert' in modes:
+        ar_main.alert_rules(tenant_sessions, modes['alert'].get('add', True), modes['alert'].get('up', True), modes['alert'].get('del', True))
+    if 'anomaly' in modes:
+        ano_sync.sync_anomaly_settings(tenant_sessions, modes['anomaly'].get('add', True), modes['anomaly'].get('up', True), modes['anomaly'].get('del', True))
+    if 'settings' in modes:
+        settings_migrate.migrate_settings(tenant_sessions)
+
+if __name__ == '__main__':
+    sync()
+
+
+# DELETION ORDER
+# Users - Roles - Resource Lists - Cloud Accounts - Account Groups
