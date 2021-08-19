a737d6cd234f8b867de333be4466e8a7a55b3768
Adam Hamilton-Sutherland <ahamiltonsut@paloaltonetworks.com>
2021-08-19 10:48:35 -0700

    Updated cloud status messages

--- cloud_accounts/cld_delete.py
+++ cloud_accounts/cld_delete.py
@@ -1,15 +1,22 @@
 from sdk.color_print import c_print
 
 def delete_accounts(cln_session, accounts):
-    for account in accounts:
-        cloud_type = ''
-        cld_id = ''
-        if 'cloudAccount' in account:
-            cloud_type = account['cloudAccount']['cloudType']
-            cld_id = account['cloudAccount']['accountId']
-        else:
-            cloud_type = account['cloudType']
-            cld_id = account['accountId']
-    
-        c_print('API - Deleting cloud account')
-        cln_session.request('DELETE', f'/cloud/{cloud_type}/{cld_id}')
+    if accounts:
+        c_print(f'Deleting Cloud Accounts from tenant \'{cln_session.tenant}\'', color='blue')
+        print()
+
+        for account in accounts:
+            cloud_type = ''
+            cld_id = ''
+            if 'cloudAccount' in account:
+                cloud_type = account['cloudAccount']['cloudType']
+                cld_id = account['cloudAccount']['accountId']
+            else:
+                cloud_type = account['cloudType']
+                cld_id = account['accountId']
+        
+            c_print('API - Deleting cloud account')
+            cln_session.request('DELETE', f'/cloud/{cloud_type}/{cld_id}')
+
+    else:
+        c_print(f'No Cloud Accounts to delete for tenant \'{cln_session.tenant}\'', color='yellow')
--- cloud_accounts/cld_update.py
+++ cloud_accounts/cld_update.py
@@ -4,33 +4,41 @@ def update_accounts(cln_session, accounts_to_update):
     '''
     Updates the details of cloud accounts on the clone tenant
     '''
-    for account in accounts_to_update:
-        cloud_type = ''
-        cld_id = ''
+    if accounts_to_update:
+        c_print(f'Updating Cloud Accounts for tenant \'{cln_session.tenant}\'', color='blue')
+        print()
 
-        if 'cloudAccount' in account:
-            cloud_type = account['cloudAccount']['cloudType']
-            cld_id = account['cloudAccount']['accountId']
-        else:
-            cloud_type = account['cloudType']
-            cld_id = account['accountId']
+        for account in accounts_to_update:
+            cloud_type = ''
+            cld_id = ''
 
-        #Patch children of orgs
-        if 'cloudAccount' in account:
-            if 'parentId' in account['cloudAccount']:
-                if account['cloudAccount']['parentId'] != None:
-                    patch = {
-                        "groupIds": account['cloudAccount']['groupIds']
-                    }
-                    status = account['cloudAccount']['enabled']
-                    if status == True:
-                        status = 'true'
-                    if status == False:
-                        status = 'false'
+            if 'cloudAccount' in account:
+                cloud_type = account['cloudAccount']['cloudType']
+                cld_id = account['cloudAccount']['accountId']
+            else:
+                cloud_type = account['cloudType']
+                cld_id = account['accountId']
 
-                    print('API - Patching child account')
-                    cln_session.request('PATCH', f'/cloud/{cloud_type}/{cld_id}', json=patch)
-                    cln_session.request('PATCH', f'/cloud/{cld_id}/status/{status}')
+            #Patch children of orgs
+            if 'cloudAccount' in account:
+                if 'parentId' in account['cloudAccount']:
+                    if account['cloudAccount']['parentId'] != None:
+                        patch = {
+                            "groupIds": account['cloudAccount']['groupIds']
+                        }
+                        status = account['cloudAccount']['enabled']
+                        if status == True:
+                            status = 'true'
+                        if status == False:
+                            status = 'false'
+
+                        print('API - Patching child account')
+                        cln_session.request('PATCH', f'/cloud/{cloud_type}/{cld_id}', json=patch)
+                        cln_session.request('PATCH', f'/cloud/{cld_id}/status/{status}')
+                    else:
+                        #Update cloud account
+                        c_print('API - Updating cloud account')
+                        cln_session.request('PUT', f'/cloud/{cloud_type}/{cld_id}', json=account)
                 else:
                     #Update cloud account
                     c_print('API - Updating cloud account')
@@ -39,8 +47,5 @@ def update_accounts(cln_session, accounts_to_update):
                 #Update cloud account
                 c_print('API - Updating cloud account')
                 cln_session.request('PUT', f'/cloud/{cloud_type}/{cld_id}', json=account)
-        else:
-            #Update cloud account
-            c_print('API - Updating cloud account')
-            cln_session.request('PUT', f'/cloud/{cloud_type}/{cld_id}', json=account)
-    return
+    else:
+        c_print(f'No Cloud Accounts to update for tenant \'{cln_session.tenant}\'', color='yellow')
