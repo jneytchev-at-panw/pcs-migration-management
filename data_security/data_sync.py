e0ec0e9f2d68e1be04fe71e0766235e7c5292c4b
Adam Hamilton-Sutherland <ahamiltonsut@paloaltonetworks.com>
2021-09-13 11:57:08 -0700

    DS test code.

---
+++ data_security/data_sync.py
@@ -0,0 +1,18 @@
+if __name__ == '__main__':
+    from sdk.load_config import load_config_create_sessions
+    from loguru import logger
+    tenant_sessions = load_config_create_sessions(True, logger)
+    
+    #Get DS info
+    res = tenant_sessions[1].request('GET', '/dashboard/data')
+    print(res.json())
+
+    #Check if DS is enabled
+    res = tenant_sessions[0].request('GET', '/api/v1/provision/dlp/status')
+    print(res.json())
+
+    #Enabled DS
+    res = tenant_sessions[1].request('GET', 'api/v1/provision/dlp')
+    print(res.json())
+    
+    
