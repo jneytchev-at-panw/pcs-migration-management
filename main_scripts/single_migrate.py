9cac8afc0ce05482489cdd7b7a4b77bf63608b9a
Adam Hamilton-Sutherland <ahamiltonsut@paloaltonetworks.com>
2021-11-12 12:00:05 -0800

    set up single migration main file

---
+++ main_scripts/single_migrate.py
@@ -0,0 +1,68 @@
+def single_migrate(tenant_sessions, entity_type, uuid, options, logger):
+    #Get entity information
+
+    try:
+        if 'cloud' == entity_type:
+            pass
+    except Exception as error:
+        logger.exception(error)
+
+    try:        
+        if 'account' == entity_type:
+            pass
+    except Exception as error:
+        logger.exception(error)
+
+    try:        
+        if 'resource' == entity_type:
+            pass
+    except Exception as error:
+        logger.exception(error)
+
+    try:    
+        if 'role' == entity_type:
+            pass
+    except Exception as error:
+        logger.exception(error)
+
+    try:    
+        if 'user' == entity_type:
+            pass
+    except Exception as error:
+        logger.exception(error)
+    
+    try:
+        if 'ip' == entity_type:
+            pass
+    except Exception as error:
+        logger.exception(error)
+    
+    try:
+        if 'compliance' == entity_type:
+            pass
+    except Exception as error:
+        logger.exception(error)
+    
+    try:
+        if 'search' == entity_type:
+            pass
+    except Exception as error:
+        logger.exception(error)
+
+    try:
+        if 'policy' == entity_type:
+            pass
+    except Exception as error:
+        logger.exception(error)
+
+    try:   
+        if 'alert' == entity_type:
+            pass
+    except Exception as error:
+        logger.exception(error)
+
+    try:    
+        if 'anomaly' == entity_type:
+            pass
+    except Exception as error:
+        logger.exception(error)
