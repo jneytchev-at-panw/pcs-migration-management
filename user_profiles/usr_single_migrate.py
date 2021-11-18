a14c27ad230a470f46d40f6847802e570e1d57fa
Adam Hamilton-Sutherland <ahamiltonsut@paloaltonetworks.com>
2021-11-18 10:29:25 -0800

    User single migrate works now

--- user_profiles/usr_single_migrate.py
+++ user_profiles/usr_single_migrate.py
@@ -11,8 +11,23 @@ def single_migrate(tenant_sessions, uuid, logger):
 
     if profile_to_add:
         #translate roles
+        tenant_user_roles = []
+        for session in tenant_sessions:
+            data = usr_get.get_user_roles(session, logger)
+            tenant_user_roles.append(data)
 
-        for session in tenant_sessions[1:]:
+        for ten_index, session in enumerate(tenant_sessions[1:]):
+            clone_roles = tenant_user_roles[ten_index + 1]
+            for index in range(len(profile_to_add['roles'])):
+                cur_id = profile_to_add['roles'][index].get('id')
+                for cln_role in clone_roles:
+                    if profile_to_add['roles'][index].get('name') == cln_role.get('name'):
+                        profile_to_add['roles'][index].update(id=cln_role.get('id'))
+                        profile_to_add['roleIds'][index] = cln_role.get('id')
+                        #Update Default Role
+                        if profile_to_add['defaultRoleId'] == cur_id:
+                            profile_to_add.update(defaultRoleId=cln_role.get('id'))
+            
             usr_add.add_users(session, [profile_to_add], logger)
     else:
         logger.warning(f'Could not find User Profile with email \'{uuid}\'')
