36c3fe8c56276c6157b594faf8bad516ebe28c79
Adam Hamilton-Sutherland <hamiltonsutherlanda@gmail.com>
2021-11-30 10:29:51 -0800

    progress on search migrate

--- saved_searches/search_single_migrate.py
+++ saved_searches/search_single_migrate.py
@@ -1,6 +1,6 @@
-def single_migrate(tenant_sessions, uuid, logger):
-   tenant_saved_searches = [] 
-   for session in tenant_sessions:
+def single_migrate(tenant_sessions, uuid, logger): 
+    tenant_saved_searches = [] 
+    for session in tenant_sessions:
         logger.debug('API - Getting saved searches for each tenant')
         querystring = {"filter":"saved"}
         res = session.request('GET', '/search/history', params=querystring)
@@ -9,7 +9,10 @@ def single_migrate(tenant_sessions, uuid, logger):
 
     source_tenant = tenant_saved_searches[0]
     clone_tenants = tenant_saved_searches[1:]
-
+    
+    search_to_add = {}
     for src_search in source_tenant:
-        print(src_search)
+        if src_search.get('id') == uuuid:
+            search_to_add = src_search
+            break
 
