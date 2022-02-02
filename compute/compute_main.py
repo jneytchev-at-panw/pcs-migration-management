dac13652150917c58d6965ee94e1d61a87ab8aea
Adam Hamilton-Sutherland <ahamiltonsut@paloaltonetworks.com>
2022-02-02 12:02:53 -0800

    Compute session manager is working

--- compute/compute_main.py
+++ compute/compute_main.py
@@ -1,22 +1,13 @@
 import compute
 from sdk.load_config import load_config_create_sessions
+from compute import compute_session_manager
 from loguru import logger
 import requests
 
 def run():
     tenant_sessions = load_config_create_sessions(True, logger)
 
+    compute_sessions = compute_session_manager.create_sessions(tenant_sessions)
 
-    session = tenant_sessions[0]
-
-    res = session.request('GET', 'meta_info')
-
-    print(res.json()['twistlockUrl'])
-    compute_url = res.json()['twistlockUrl']
-    compute_url += '/api/v1/authenticate'
-
-    compute_session = requests.request('POST', compute_url, json={"username":None,"password":None,"token":session.token})
-
-    compute_token = compute_session.json()['token']
-
-    print(compute_token)
+    res = compute_sessions[0].request('GET', 'api/v1/groups')
+    print(res.json())
